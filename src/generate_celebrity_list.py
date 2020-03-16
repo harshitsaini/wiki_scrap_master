1c817dac9cbbc7af43b9254177fb649c6adec06d
Harshit Saini <harshit.saini@widas.in>
2020-03-16 19:13:22 +0530

    added basic profiling

--- src/generate_celebrity_list.py
+++ src/generate_celebrity_list.py
@@ -29,63 +29,72 @@ def get_celebrities_data():
     ...
 
 
-def main():
-    # pprint(actors.get_specific_roles_or_genres_links())
-    # pprint(actors.get_nationality_links())
-    # pprint(actors.get_other_links())
+def main2():
+    data = actors.get_specific_roles_or_genres_links()
+    data = actors.get_nationality_links()
+    data = actors.get_other_links()
 
-    # pprint(guest_stars_of_the_simpsons.get_guest_star_links())
-    # pprint(guest_stars_of_the_simpsons.get_guest_star_animators_links())
+    data = guest_stars_of_the_simpsons.get_guest_star_links()
+    data = guest_stars_of_the_simpsons.get_guest_star_animators_links()
 
-    # pprint(guest_stars_of_sesame_street.get_guest_star_links())
+    data = guest_stars_of_sesame_street.get_guest_star_links()
 
-    # pprint(inventors.get_inventors_links())
+    data = inventors.get_inventors_links()
 
-    # pprint(celebrities_with_advanced_degrees.get_celebrity_links())
+    data = celebrities_with_advanced_degrees.get_celebrity_links()
 
-    # pprint(comedians.get_comedians_links())
+    data = comedians.get_comedians_links()
 
-    # pprint(dance_personalities.get_dance_personalities_links())
-    # pprint(dance_personalities.get_ballet_dance_personalities_links())
+    data = dance_personalities.get_dance_personalities_links()
+    data = dance_personalities.get_ballet_dance_personalities_links()
 
-    # pprint(dancers.get_dance_personalities_links())
+    data = dancers.get_dance_personalities_links()
 
-    # pprint(fashion_designers.get_fashion_designers_links())
-    # pprint(fashion_designers.get_fashion_designers_links2())
+    data = fashion_designers.get_fashion_designers_links()
+    data = fashion_designers.get_fashion_designers_links2()
 
-    # pprint(female_boxers.get_female_boxers_links())
+    data = female_boxers.get_female_boxers_links()
 
-    # pprint(film_and_television_directors.get_film_and_television_directors_links())
+    data = film_and_television_directors.get_film_and_television_directors_links()
 
-    # pprint(forbes_celebrity_100.get_forbes_celebrity_100_links())
+    data = forbes_celebrity_100.get_forbes_celebrity_100_links()
 
-    # pprint(league_of_legends_players.get_league_of_legends_players_links())
+    data = league_of_legends_players.get_league_of_legends_players_links()
 
-    # pprint(male_boxers.get_male_boxers_links())
+    data = male_boxers.get_male_boxers_links()
 
-    # pprint(news_presenters.get_news_presenters_links())
-    # pprint(news_presenters.get_news_presenters_links2())
+    data = news_presenters.get_news_presenters_links()
+    data = news_presenters.get_news_presenters_links2()
 
-    # pprint(singer_songwriters.get_singer_songwriters_links())
-    # pprint(singer_songwriters.get_singer_songwriters_links2())
+    data = singer_songwriters.get_singer_songwriters_links()
+    data = singer_songwriters.get_singer_songwriters_links2()
 
-    # pprint(social_thinkers.get_social_thinkers_links())
+    data = social_thinkers.get_social_thinkers_links()
 
-    # pprint(stars_on_hollywood_walk_of_fame.get_stars_on_hollywood_walk_of_fame_links())
+    data = stars_on_hollywood_walk_of_fame.get_stars_on_hollywood_walk_of_fame_links()
 
-    # pprint(bullfighters.get_bullfighters_links())
+    data = bullfighters.get_bullfighters_links()
 
-    # pprint(avenue_of_stars_london.get_avenue_of_stars_london_links())
+    data = avenue_of_stars_london.get_avenue_of_stars_london_links()
 
-    # pprint(tattoo_artists.get_tattoo_artists_links())
+    data = tattoo_artists.get_tattoo_artists_links()
 
-    # pprint(television_presenters.get_television_presenters_links())
-    # pprint(television_presenters.get_television_presenters_links2())
+    data = television_presenters.get_television_presenters_links()
 
-    # pprint(youtubers.get_youtubers_links())
+    data = television_presenters.get_television_presenters_links2()
+
+    data = youtubers.get_youtubers_links()
+
+    # pprint(data)
     ...
 
 
 # Driver Function
 if __name__ == '__main__':
-    main()
+    import cProfile
+    cProfile.run('main2()', 'restats')
+    import pstats
+    p = pstats.Stats('restats')
+    # p.strip_dirs().sort_stats(-1).print_stats()
+    p.sort_stats('cumulative').print_stats("Celeb_Data_Analytics/wiki_scrap_master*", 10)
+
